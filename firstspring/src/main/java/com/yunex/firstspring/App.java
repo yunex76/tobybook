0fbfb837db714cfa7ebe9c78e7e6cbe9c468f08f
yunex76 <yunex76@gmail.com>
2015-05-04 17:45:26 +0900

    1. firstspring project commit

---
+++ firstspring/src/main/java/com/yunex/firstspring/App.java
@@ -0,0 +1,13 @@
+package com.yunex.firstspring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
