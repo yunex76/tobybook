8e96e92245386140d23e06f28aa0298c9c1f5b7d
yunex76 <yunex76@gmail.com>
2015-05-06 17:37:22 +0900

    1. AppTest class 추가 2. UserDao class 수정

---
+++ firstspring/src/main/java/com/yunex/firstspring/AppTest.java
@@ -0,0 +1,38 @@
+package com.yunex.firstspring;
+
+import java.sql.SQLException;
+
+import com.yunex.firstspring.dao.ConnectionMaker;
+import com.yunex.firstspring.dao.DConnectionMaker;
+import com.yunex.firstspring.dao.UserDao;
+import com.yunex.firstspring.domain.User;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTest 
+{
+    public static void main( String[] args ) throws ClassNotFoundException, SQLException
+    {
+    	System.out.println("AppTest run.............");
+    	
+    	ConnectionMaker connectionMaker = new DConnectionMaker();
+    	UserDao dao = new UserDao(connectionMaker);
+    	
+    	User user = new User();
+    	user.setId("whiteship");
+    	user.setName("백기선");
+    	user.setPassword("married");
+    	
+    	dao.add(user);
+    	
+    	System.out.println(user.getId() + " 등록 성공!!");
+    	
+    	User user2 = dao.get(user.getId());
+    	System.out.println(user2.getName());
+    	System.out.println(user2.getPassword());
+    	
+    	System.out.println(user2.getId() + " 조회 성공!!!");
+    }
+}
--- firstspring/src/main/java/com/yunex/firstspring/dao/UserDao.java
+++ firstspring/src/main/java/com/yunex/firstspring/dao/UserDao.java
@@ -15,6 +15,10 @@ public class UserDao {
 		connectionMaker = new DConnectionMaker();
 	}
 	
+	public UserDao(ConnectionMaker connectionMaker) {
+		this.connectionMaker = connectionMaker;
+	}
+	
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		
 		Connection c = connectionMaker.makeConnection();
