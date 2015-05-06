a50581980f685d45bdbc47c94dcf28b416fa2cdb
yunex76 <yunex76@gmail.com>
2015-05-06 15:56:34 +0900

    1. UserDao class 수정  - DB연결부분 분리

--- firstspring/src/main/java/com/yunex/firstspring/dao/UserDao.java
+++ firstspring/src/main/java/com/yunex/firstspring/dao/UserDao.java
@@ -12,8 +12,7 @@ public class UserDao {
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		
-		Class.forName("org.hsqldb.jdbc.JDBCDriver");
-		Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/", "sa", "");
+		Connection c = getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users (id, name, password) values (?, ?, ?)");
 		ps.setString(1, user.getId());
@@ -29,9 +28,8 @@ public class UserDao {
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		
-		Class.forName("org.hsqldb.jdbc.JDBCDriver");
-		Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/", "sa", "");
-
+		Connection c = getConnection();
+		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		
@@ -50,4 +48,12 @@ public class UserDao {
 	
 		return user;
 	}
+
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("org.hsqldb.jdbc.JDBCDriver");
+		Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/", "sa", "");
+		
+		return c;
+	}
+
 }
